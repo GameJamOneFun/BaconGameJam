7409e13006cf303c1750361d5722bc54936a960c
mikeysboy <tylerjunkmail1994@gmail.com>
2013-06-07 16:34:00 -0700

    Update Flashlight.java

--- BaconGameJamSlam/Flashlight.java
+++ BaconGameJamSlam/Flashlight.java
@@ -1,4 +1,20 @@
 public class Flashlight extends Actor
 {
-
+    private boolean found;
+    
+    /*public FlashLight(Maze maze)
+    {
+        
+    }*/
+    
+    public boolean found()
+    {
+        return found;
+    }
+    
+    public void setFound()
+    {
+        found = true;
+    }
+    
 }
