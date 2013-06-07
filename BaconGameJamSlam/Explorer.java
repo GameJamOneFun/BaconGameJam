1a3dd3c89f2ffc248faf748339b2e4feadd4b965
Miguel Aguilar <aguilar.miguel10@gmail.com>
2013-06-07 17:21:16 -0600

    Update Explorer.java

--- BaconGameJamSlam/Explorer.java
+++ BaconGameJamSlam/Explorer.java
@@ -1,9 +1,44 @@
 public class Explorer extends Actor
 {
-
+  private double lightRadius;
+  private int flashLights;
+  private boolean inView;
+  
   public Explorer()
   {
+    this.inView = false;
+    this.flashLights = 0;
+    this.lightradius = 20.0;
+  }
   
+  public double getLightRadius()
+  {
+    return this.lightRadius;
+  }
+  
+  public void setLightRadius(double r)
+  {
+    this.lightRadius = r;
+  }
+  
+  public int getFlashLights()
+  {
+    return this.flashLights;
+  }
+  
+  public void setFlashLights(int l)
+  {
+    this.flashLights = l;
+  }
+  
+  public boolean getInView()
+  {
+    return this.inView;
+  }
+  
+  public void setInView(boolean v)
+  {
+    this.inView = v;
   }
   
 }
