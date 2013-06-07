fd21c4b6316f9eefb77dd3638a565dab8a1f8fcd
reececamper@cox.net <reececamper@cox.net>
2013-06-07 16:04:40 -0700

    Changed Background Size

--- BaconGameJamSlam/Background.java
+++ BaconGameJamSlam/Background.java
@@ -16,7 +16,7 @@ public class Background extends World
     public Background()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 600, 1); 
     }
     
     //Comment here
