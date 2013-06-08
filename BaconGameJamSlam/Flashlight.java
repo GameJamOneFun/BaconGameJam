e0b0d818982327a9485e3a1b7f0c566e972202d4
Reece Camper <reececamper@cox.net>
2013-06-07 18:27:25 -0600

    Update Flashlight.java

--- BaconGameJamSlam/Flashlight.java
+++ BaconGameJamSlam/Flashlight.java
@@ -1,20 +1,27 @@
 public class Flashlight extends Actor
 {
-    private boolean found;
-    
-    public FlashLight(Maze maze)
+    it j = 0;
+    public void act() 
     {
-        
+        createPortal(250, 250, j); //Code credit from greenfoot user 'danpost'. All credit goes to him!
     }
     
-    public boolean found()
-    {
-        return found;
-    }
-    
-    public void setFound()
-    {
-        found = true;
+    public void createPortal(int x, int y, int r) // x and y are the center and r  is the radius  
+    {  
+        GreenfootImage img = getImage();  
+        Color color = Color.white;  
+        Color trans = new Color(0, 0, 0, 0);  
+        img.setColor(color);  
+        img.fillOval(x - r, y - r, r * 2, r * 2);  
+        for (int i = 0; i < r * 2; i++)  
+        {  
+            for (int j = 0; j < r * 2; j++)  
+            {  
+                Color here = img.getColorAt(x - r + i, y - r + j);  
+                if (color.equals(here)) img.setColorAt(x - r + i, y - r + j, trans);  
+            }  
+        }  
+        setImage(img);  
     }
     
 }
