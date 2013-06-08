5c15dcb0cc368419de58e72203eb8a93d8453a75
Reece Camper <reececamper@cox.net>
2013-06-07 18:28:52 -0600

    Create Battery.java

---
+++ BaconGameJamSlam/Battery.java
@@ -0,0 +1,19 @@
+public class Battery extends Actor
+{
+  private boolean found;
+   
+  public FlashLight(Maze maze)
+  {
+      
+  }
+  
+  public boolean found()
+  {
+      return found;
+  }
+  
+  public void setFound()
+  {
+      found = true;
+  }
+}
